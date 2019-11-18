10fd90376d979585b422b820f5b26bd9367ec51d
sashodada <a.shumakov97@gmail.com>
2019-11-18 21:51:06 +0200

    added another testData file, refactored

--- hw3/81489.cpp
+++ hw3/81489.cpp
@@ -1,3 +1,9 @@
+/**
+ * Genetic algorithm
+ * 3rd homework for AI course @FMI, Sofia University
+ * Aleksandar Shumakov, FN 81489
+ **/
+
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -9,11 +15,6 @@
 
 using namespace std;
 
-class Chromosome {
-    public:
-        Chromosome() {}
-};
-
 void parseLine(string &line, vector<int> &cost, vector<int> &value)
 {
     int spaceIndex = line.find(' ');
@@ -23,35 +24,67 @@ void parseLine(string &line, vector<int> &cost, vector<int> &value)
 
 void input(vector<int> &cost, vector<int> &value)
 {
-    ifstream input("testData.txt", ifstream::in);
+    ifstream input("testData_long.txt", ifstream::in);
     string line;
 
-    while(getline(input, line)) {
+    getline(input, line);
+    while (getline(input, line))
+    {
         parseLine(line, cost, value);
+        cout << cost[cost.size() - 1] << endl; 
     }
 }
 
-vector<bool> initChromosome(int length)
+vector<bool> initChromosome(int chromosomeLength)
 {
-    vector<bool> chromosome(length);
-    for (int i = 0; i < length; ++i)
+    vector<bool> chromosome(chromosomeLength);
+    for (int i = 0; i < chromosomeLength; ++i)
     {
-        chromosome[i] = rand() % 2;
+        chromosome[i] = rand() % 10 == 0;
     }
     return chromosome;
 }
 
-int totalValue(vector<bool> &chromosome, const vector<int> &cost, const vector<int> &value, int size)
+int totalValue(vector<bool> &chromosome, const vector<int> &cost, const vector<int> &value, int chromosomeSize)
 {
     int totalCost = 0;
     int totalValue = 0;
-    for (int i = 0; i < size; ++i)
+    for (int i = 0; i < chromosomeSize; ++i)
     {
-        totalCost += chromosome[i] * cost[i];
-        totalValue += chromosome[i] * value[i];
+        totalCost += chromosome[i] ? cost[i] : 0;
+        totalValue += chromosome[i] ? value[i] : 0;
     }
 
     return totalCost > 5000 ? 5000 - totalCost : totalValue;
+}
+
+void generateChildren(vector<bool> &parent1, vector<bool> &parent2, vector<vector<bool>> &population, int startIndex, int chromosomeLength)
+{
+    for (int i = 0; i < 2; ++i)
+    {
+        vector<bool> chromosome(chromosomeLength);
+        for (int j = 0; j < chromosomeLength; ++j)
+        {
+            chromosome[j] = rand() % 2 ? parent1[j] : parent2[j];
+        }
+        population[startIndex + i] = chromosome;
+    }
+}
+
+void mutatePopulation(vector<vector<bool>> &population, int populationSize, int chromosomeLength)
+{
+    for (int i = 0; i < populationSize; ++i)
+    {
+        // Chromosome mutation change is 10% here
+        if (rand() % 100 < 10)
+        {
+            for (int j = 0; j < 3; ++j)
+            {
+                int bitIndex = rand() % chromosomeLength;
+                population[i][bitIndex] = !population[i][bitIndex];
+            }
+        }
+    }
 }
 
 void solve(vector<int> &cost, vector<int> &value)
@@ -62,27 +95,39 @@ void solve(vector<int> &cost, vector<int> &value)
     int CHROMOSOME_LENGTH = cost.size();
 
     auto chromosomeComparator = [cost, value, CHROMOSOME_LENGTH](vector<bool> &left, vector<bool> &right) {
-        return totalValue(left, cost, value, CHROMOSOME_LENGTH) < totalValue(right, cost, value, CHROMOSOME_LENGTH);
+        return totalValue(left, cost, value, CHROMOSOME_LENGTH) > totalValue(right, cost, value, CHROMOSOME_LENGTH);
     };
-    priority_queue<vector<bool>, vector<vector<bool>>, decltype(chromosomeComparator)> population(chromosomeComparator);
+
+    vector<vector<bool>> population(POPULATION_SIZE);
 
     for (int i = 0; i < POPULATION_SIZE; ++i)
     {
-        population.push(initChromosome(CHROMOSOME_LENGTH));
+        population[i] = initChromosome(CHROMOSOME_LENGTH);
     }
 
-    for (int i = 0; i < POPULATION_SIZE; ++i)
+    sort(population.begin(), population.end(), chromosomeComparator);
+
+    for (int i = 0; i < 5000; i++)
     {
-        vector<bool> top = population.top();
-        cout << totalValue(top, cost, value, CHROMOSOME_LENGTH) << " ";
-        population.pop();
+        cout << "ITERATION: " << i + 1 << endl;
+        int killCount = (POPULATION_SIZE / 10) * 2;
+        for (int i = 0; i < killCount; i += 2)
+        {
+            auto parent1 = population[i];
+            auto parent2 = population[i + 1];
+
+            generateChildren(parent1, parent2, population, POPULATION_SIZE - killCount + i, CHROMOSOME_LENGTH);
+        }
+        mutatePopulation(population, POPULATION_SIZE, CHROMOSOME_LENGTH);
+
+        sort(population.begin(), population.end(), chromosomeComparator);
+        cout << "\n\n";
+        cout << "FITTEST CHROMOSOME: " << totalValue(population[0], cost, value, CHROMOSOME_LENGTH) << "\n";
     }
-    cout << endl;
 }
 
 int main()
 {
-
     vector<int> cost;
     vector<int> value;
     input(cost, value);
